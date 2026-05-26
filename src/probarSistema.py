668a7983bad01ebe3303870ce90ae3cda84f001a
pablooreugr <pabloore@correo.ugr.es>
2026-05-26 15:51:48 +0200

    creo el python para las pruebas sinteticas, en este caso sin fijar el campo magnético

--- src/probarSistema.py
+++ src/probarSistema.py
@@ -1,1 +1,16 @@
-print('hola')
+import magnetismo as mag
+import deconvolucion as deco
+import numpy as np
+
+
+
+
+
+if __name__ == "__main__":
+
+    # Ruta en tu workspace
+    path = "data/datos_sunspot.npz"  # o usa el enlace [data/datos_sunspot.npz](data/datos_sunspot.npz)
+    
+    with np.load(path, allow_pickle=True) as npz:
+        lambdas = npz['lam']
+        datos = npz['stokes']
